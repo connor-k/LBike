2893310ff9ec785f2358865684095f27532f9c74
Nolan Miller <namiller@usc.edu>
2014-04-26 18:18:37 -0700

    added keyboard support

---
+++ Lightbike_Project/pulse_gen.v
@@ -0,0 +1,11 @@
+module pulse_gen(output reg pulse_out, input trigger_in, input clk);
+reg delay;
+
+always @ (posedge clk)
+begin
+    if (trigger_in && !delay) pulse_out <= 1'b1;
+    else pulse_out <= 1'b0;
+    delay <= trigger_in;
+end 
+
+endmodule
